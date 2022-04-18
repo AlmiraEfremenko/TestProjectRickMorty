14e1799d98b560973275d57fa6d873f61661a8e1
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-04-18 11:18:59 +0500

    Add settings window and delete storyboard.

---
+++ TestProjectRickMorty/Controller/ViewControllerDetail.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  TestProjectRickMorty
+//
+//  Created by MAC on 18.04.2022.
+//
+
+import Foundation
---
+++ TestProjectRickMorty/View/TableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  TableViewCell.swift
+//  TestProjectRickMorty
+//
+//  Created by MAC on 18.04.2022.
+//
+
+import Foundation
